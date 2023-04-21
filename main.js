9e63ea08d19352fc3a1c80b11a2e3e68a6db9978
vyominiiiiiiii <102034432+vyominiiiiiiii@users.noreply.github.com>
2023-04-22 00:46:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+var date=new Date();
+console.log(date);
+document.getElementById("date").innerHTML=date;
+
+//var day=date.getDay();
+//console.log(day);
+//function one(){
+   // console.log(1);
+//var x=1;
+//document.getElementById("userInput").innerHTML=1;
+//}
+
+function type(){
+
+   switch(id){
+       
+   case one:
+   console.log(1);
+   break;
+   
+   case two:
+   console.log(2);
+   break;
+   
+   case three:
+   console.log(3);
+   break;
+   
+   case four:
+   console.log(4);
+   break;
+   
+   case five:
+   console.log(5);
+   break;
+   
+   case six:
+   console.log(6);
+   break;
+   
+   case seven:
+   console.log(7);
+   break;
+   
+   case eight:
+   console.log(8);
+   break;
+   
+   case nine:
+   console.log(9);
+   break;
+   
+   case zero:
+   console.log(0);
+   break;
+   
+   default:
+   console.log("None");
+   
+   }
+   
+   }
+
+
+
+
+
+
+
+
+
+
